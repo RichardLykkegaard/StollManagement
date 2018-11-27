d6f6bc49e430a603b4818a4f4f3d40e40a44e08f
Richard Lykkegaard Guldbrandsen <rich0226@student.eadania.dk>
2018-11-27 12:14:19 +0100

    Added PagesController, composer & routes

--- routes/web.php
+++ routes/web.php
@@ -10,7 +10,14 @@
 | contains the "web" middleware group. Now create something great!
 |
 */
-
 Route::get('/', function () {
     return view('index');
 });
+
+/*
+Route::get('/', function () {
+    return view('index');
+});
+
+Route::get('/index','PagesController@index');
+*/
