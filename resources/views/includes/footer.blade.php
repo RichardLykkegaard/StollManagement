10b1ac58e8533d31d46776614f4008eb99754130
Richard Lykkegaard Guldbrandsen <rich0226@student.eadania.dk>
2018-11-28 13:47:51 +0100

    Moved graph scripts from footer to index

--- resources/views/includes/footer.blade.php
+++ resources/views/includes/footer.blade.php
@@ -11,37 +11,5 @@
     <script>
       feather.replace()
     </script>
-
-    <!-- Graphs -->
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.7.1/Chart.min.js"></script>
-    <script>
-      var ctx = document.getElementById("myChart");
-      var myChart = new Chart(ctx, {
-        type: 'line',
-        data: {
-          labels: ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"],
-          datasets: [{
-            data: [15339, 21345, 18483, 24003, 23489, 24092, 12034],
-            lineTension: 0,
-            backgroundColor: 'transparent',
-            borderColor: '#2F4F4F',
-            borderWidth: 4,
-            pointBackgroundColor: '#2F4F4F'
-          }]
-        },
-        options: {
-          scales: {
-            yAxes: [{
-              ticks: {
-                beginAtZero: true
-              }
-            }]
-          },
-          legend: {
-            display: false,
-          }
-        }
-      });
-    </script>
   </body>
 </html>
--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -72,7 +72,7 @@
 
         <main role="main" class="col-md-9 ml-sm-auto col-lg-10 px-4">
           <div class="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3 border-bottom">
-            <h1 class="h2">Dashboard</h1>
+            <h1 class="h2 text-success"><b>Revenue</b></h1>
             <div class="btn-toolbar mb-2 mb-md-0">
               <div class="btn-group mr-2">
                 <button class="btn btn-sm btn-outline-secondary">Share</button>
@@ -218,5 +218,36 @@
         </main>
       </div>
     </div>
+<!-- Graphs -->
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.7.1/Chart.min.js"></script>
+    <script>
+      var ctx = document.getElementById("myChart");
+      var myChart = new Chart(ctx, {
+        type: 'line',
+        data: {
+          labels: ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"],
+          datasets: [{
+            data: [11339, 14345, 18483, 21003, 23489, 25092, 32034],
+            lineTension: 0,
+            backgroundColor: 'transparent',
+            borderColor: '#2F4F4F',
+            borderWidth: 4,
+            pointBackgroundColor: '#2F4F4F'
+          }]
+        },
+        options: {
+          scales: {
+            yAxes: [{
+              ticks: {
+                beginAtZero: true
+              }
+            }]
+          },
+          legend: {
+            display: false,
+          }
+        }
+      });
+    </script>
 @include('includes.footer')
 
