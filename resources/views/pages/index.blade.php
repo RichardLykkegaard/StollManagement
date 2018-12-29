357aedf2f85d5c2d4b7f244b199dd1d8b6d191e8
Richard Lykkegaard Guldbrandsen <rich0226@student.eadania.dk>
2018-12-29 17:11:59 +0100

    Update index.blade.php

--- resources/views/pages/index.blade.php
+++ resources/views/pages/index.blade.php
@@ -36,8 +36,6 @@
                             Customers
                         </a>
                     </li>
-
-
                 </ul>
 
                 <h6 class="sidebar-heading d-flex justify-content-between align-items-center px-3 mt-4 mb-1 text-muted">
